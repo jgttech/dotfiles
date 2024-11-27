fa0eccec9af34223b298d59403f06acf7929d3a1
Jonathon Tech <jgt.tech@protonmail.com>
2024-11-26 22:53:00 -0600

    Setup Alacritty not to have a default

---
+++ .bin/alacritty/Copy.go
@@ -0,0 +1,32 @@
+package alacritty
+
+import (
+	"errors"
+	"fmt"
+	"jgttech/dotfiles/assert"
+	"jgttech/dotfiles/env"
+	"jgttech/dotfiles/exec"
+	"jgttech/dotfiles/os"
+	_os "os"
+
+	"path"
+	"runtime"
+)
+
+func Copy() {
+	name := "alacritty." + runtime.GOOS + ".toml"
+	base := path.Join(env.BASE, "alacritty", ".config", "alacritty")
+	cfg := path.Join(base, "alacritty.toml")
+	target := path.Join(base, name)
+
+	if os.Exists(cfg) {
+		_os.Remove(cfg)
+	}
+
+	if !os.Exists(target) {
+		panic(errors.New(fmt.Sprintf("Alacritty config not found: '%s'", target)))
+	}
+
+	cmd := exec.Cmd(fmt.Sprintf("cp -v %s %s", target, cfg), exec.Stdio)
+	assert.Will(cmd.Run())
+}
--- .bin/cmds/install/main.go
+++ .bin/cmds/install/main.go
@@ -2,10 +2,8 @@ package install
 
 import (
 	"context"
-	"fmt"
 	"jgttech/dotfiles/alacritty"
 	"jgttech/dotfiles/stow"
-	"runtime"
 
 	"github.com/urfave/cli/v3"
 )
@@ -14,14 +12,7 @@ func Command() *cli.Command {
 	return &cli.Command{
 		Name: "install",
 		Action: func(ctx context.Context, c *cli.Command) error {
-			fmt.Println("goos:", runtime.GOOS)
-
-			if runtime.GOOS == "darwin" {
-				alacritty.Swap("alacritty.darwin.toml")
-			} else if runtime.GOOS == "linux" {
-				alacritty.Swap("alacritty.linux.toml")
-			}
-
+			alacritty.Copy()
 			stow.Link()
 
 			return nil
