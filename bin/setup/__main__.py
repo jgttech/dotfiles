61338302e4a726f667abb099c8962b017c1e7ef2
Jonathon Tech <jgt.tech@protonmail.com>
2025-02-09 00:35:22 -0600

    Updates

--- bin/setup/__main__.py
+++ bin/setup/__main__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-from core.os import installed
+from core.os import installed, get_bin
 from core.argv import parse
 
 if __name__ == "__main__":
   argv = parse()
+  dir = get_bin()
 
-  print(f"dev..: {argv.dev}")
-  print(f"lang.: {argv.lang}")
-  print(f"path.: {argv.path}")
-  print(f"git..: {installed("git")}")
+  print(f"dev......: {argv.dev}")
+  print(f"lang.....: {argv.lang}")
+  print(f"path.....: {argv.path}")
+  print(f"bin......: {dir}")
+  print(f"git......: {installed("git")}")
--- bin/setup/core/os.py
+++ bin/setup/core/os.py
@@ -1,4 +1,17 @@
+from os import access, environ, path, W_OK, R_OK
 from subprocess import run
 
 def installed(pkg: str) -> bool:
-  return bool(run(f"zsh -i -c \"{pkg}\"", shell=True, capture_output=True).returncode)
+  return not bool(run(f"zsh -i -c \"which {pkg}\"", shell=True, capture_output=True).returncode)
+
+def get_bin() -> str:
+  HOME = environ["HOME"]
+  PATH = environ["PATH"]
+  BIN = path.join(HOME, ".local/bin")
+
+  if BIN not in PATH:
+    for dir in PATH.split(":"):
+      if access(dir, W_OK|R_OK) and "sbin" not in dir:
+        return dir
+
+  return BIN
