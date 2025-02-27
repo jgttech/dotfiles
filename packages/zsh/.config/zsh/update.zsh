945d1ad34ac309ff2490c7227a48ca8d7998ca06
Jonathon Tech <jgt.tech@protonmail.com>
2025-02-27 00:56:10 -0600

    Update auto-update functionality in ZSH

--- packages/zsh/.config/zsh/update.zsh
+++ packages/zsh/.config/zsh/update.zsh
@@ -3,35 +3,19 @@
 # within a scoped context so that it does NOT
 # pollute the global context.
 dotfiles_update() {
-  local updated=false
-  local cwd=`pwd`
-  local home=`dotfiles_json ".home"`
-  local tools=`dotfiles_json ".tools"`
-  local cli=`dotfiles_json ".cli"`
+  local cwd=$(pwd)
+  local home=$(dotfiles_json ".home")
+  local tools=$(dotfiles_json ".tools")
+  local cli=$(dotfiles_json ".cli")
 
   cd "${home}"
 
-  local current_hash=`git rev-parse HEAD`
-  echo "CURRENT HASH: ${current_hash}"
+  pull_output=$(git pull)
 
-  git pull &> /dev/null
-
-  local update_hash=`git rev-parse HEAD`
-  echo "UPDATE HASH: ${update_hash}"
-
-  if [[ "${current_hash}" != "${update_hash}" ]]; then
-    updated=true
-  fi
-
-  # If there was an updated change, the command
-  # used to build the CLI is re-run exactly as it
-  # was when the inital install happened, which will
-  # update the CLI to a newer version.
-  if [[ "${updated}" == true ]]; then
+  if [[ "$pull_output" != *"Already up to date"* ]]; then
     echo "Updating, please wait..."
     cd "${home}/${tools}"
-    eval "${cli}"
-    clear
+    ${cli}
   fi
 
   cd "${cwd}"
