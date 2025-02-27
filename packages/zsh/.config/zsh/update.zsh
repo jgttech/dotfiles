794ffa5b24d26fd794d23e5a47f986fe16ce5900
Jonathon Tech <jgt.tech@protonmail.com>
2025-02-27 03:03:56 -0600

    Update auto-update functionality to work with macOS better

--- packages/zsh/.config/zsh/update.zsh
+++ packages/zsh/.config/zsh/update.zsh
@@ -3,6 +3,9 @@
 # within a scoped context so that it does NOT
 # pollute the global context.
 dotfiles_update() {
+  # Check if python3 exists and use it, otherwise fallback to python
+  local python_cmd=$(command -v python3 || command -v python)
+
   local cwd=$(pwd)
   local home=$(dotfiles_json ".home")
   local tools=$(dotfiles_json ".tools")
@@ -15,7 +18,12 @@ dotfiles_update() {
   if [[ "$pull_output" != *"Already up to date"* ]]; then
     echo "Updating, please wait..."
     cd "${tools}"
-    eval "${cli}"
+
+    # Replacing 'python' in the cli command with the appropriate python command
+    local compat_cli=$(echo "$cli" | sed "s/^python/$python_cmd/")
+
+    # Execute the command
+    eval "$compat_cli"
   fi
 
   cd "${cwd}"
