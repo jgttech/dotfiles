eca786117440baedf4cbec1b4b88ee73664d6209
Jonathon Tech <jgt.tech@protonmail.com>
2025-03-02 00:03:44 -0600

    WIP (1740895424) Testing updates to the CLI

--- tools/go/main.go
+++ tools/go/main.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/urfave/cli/v3"
 	"jgttech/dotfiles/cmds/install"
-	"jgttech/dotfiles/cmds/purge"
 	"jgttech/dotfiles/cmds/version"
 	"jgttech/dotfiles/src/cfg"
 )
@@ -18,7 +17,6 @@ func main() {
 		Commands: []*cli.Command{
 			install.Command(build),
 			version.Command(build),
-			purge.Command(build),
 		},
 	}
 
