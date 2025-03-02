bbaf05c862f4e434546ce3114a408487cfe7e7d6
Jonathon Tech <jgt.tech@protonmail.com>
2025-03-02 00:34:21 -0600

    Test uninstall

--- tools/go/cmds/uninstall/main.go
+++ tools/go/cmds/uninstall/main.go
@@ -6,6 +6,7 @@ import (
 	"jgttech/dotfiles/src/cfg"
 	"jgttech/dotfiles/src/exec"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/urfave/cli/v3"
@@ -21,7 +22,11 @@ func Command(build *cfg.Build) *cli.Command {
 		Usage: "Removes the system packages and resets the ZSHRC file. Does NOT uninstall the CLI.",
 		Action: func(ctx context.Context, c *cli.Command) error {
 			packages := strings.Join(build.Packages, " ")
-			stow := exec.Cmd(fmt.Sprintf("stow -t %s -D %s", HOME, packages), exec.Dir(HOME))
+			packagesDir := path.Join(build.Home, "packages")
+			stow := exec.Cmd(
+				fmt.Sprintf("stow -t %s -D %s", HOME, packages),
+				exec.Dir(packagesDir),
+			)
 
 			if err := stow.Run(); err != nil {
 				return err
