e85a2776d803f15c01e9577003cdc24b749d4e83
Jonathon Tech <jgt.tech@protonmail.com>
2025-03-31 21:28:35 -0600

    Remove unnecessary Terraform autocomplete from Arch Linux ZSH files.

--- packages/zsh/.config/zsh/os/archlinux.zsh
+++ packages/zsh/.config/zsh/os/archlinux.zsh
@@ -2,9 +2,3 @@
 [[ -d "/home/linuxbrew/.linuxbrew" ]] && {
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)";
 }
-
-# Terraform autocomplete
-if installed "terraform"; then
-  autoload -U +X bashcompinit && bashcompinit
-  complete -o nospace -C /usr/local/bin/terraform terraform
-fi
