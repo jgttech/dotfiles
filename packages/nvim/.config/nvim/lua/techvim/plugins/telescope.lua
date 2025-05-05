01f177c8f387558bac200595f47b3f1a8159cd3f
Jonathon Tech <jgt.tech@protonmail.com>
2025-05-04 20:37:27 -0600

    Update telescope search to work better

--- packages/nvim/.config/nvim/lua/techvim/plugins/telescope.lua
+++ packages/nvim/.config/nvim/lua/techvim/plugins/telescope.lua
@@ -40,14 +40,14 @@ return {
           -- Include hidden files (dot files) and ignore .git
           hidden = true, -- Show hidden files (e.g., .config, .gitignore)
           no_ignore = true, -- Ignore .gitignore and include all files
-          file_ignore_patterns = { "^.git/" }, -- Exclude .git directory
+          file_ignore_patterns = { "^.git/", "^node_modules/" }, -- Exclude .git directory
         },
         live_grep = {
           -- Enable searching in hidden files for live_grep
           additional_args = function()
             return { "--hidden", "--no-ignore" }
           end,
-          file_ignore_patterns = { "^.git/" }, -- Exclude .git directory
+          file_ignore_patterns = { "^.git/", "^node_modules/" }, -- Exclude .git directory
         },
       },
       extensions = {
@@ -70,7 +70,7 @@ return {
     vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
     vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
     vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-    vim.keymap.set("n", "<leader>sg", "<cmd>Telescope live_grep<cr>", { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
     vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
     vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
     vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
