2d224f18518d03321b5913d43bf7843e1cd21989
Jonathon Tech <59492@icf.com>
2025-05-13 16:53:42 -0500

    Add support for 'postgrestools' for all the platforms I use

--- packages/zsh/.config/zsh/os/main.zsh
+++ packages/zsh/.config/zsh/os/main.zsh
@@ -1,10 +1,8 @@
 #!/usr/bin/env zsh
+export PATH="$HOME/.config/nvim/bin:$PATH"
+
 if [[ $(uname) == "Darwin" ]]; then
   source "${DOTFILES_ZSHRC}/os/darwin.zsh"
 elif command -v pacman &> /dev/null; then
   source "${DOTFILES_ZSHRC}/os/archlinux.zsh"
-fi
-
-if [[ "$(uname -p)" == "arm" ]]; then
-  export PATH="$HOME/.config/nvim/bin:$PATH"
 fi
