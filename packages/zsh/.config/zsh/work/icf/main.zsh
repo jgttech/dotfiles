a914c5a015c2f346a6c10c85ce69af50594822f0
Jonathon Tech <59492@icf.com>
2025-05-15 16:14:06 -0500

    Update ZSH files to link ICF files into dotfiles for work

--- packages/zsh/.config/zsh/work/icf/main.zsh
+++ packages/zsh/.config/zsh/work/icf/main.zsh
@@ -1,1 +1,8 @@
 #!/usr/bin/env zsh
+for file in "${DOTFILES_ZSHRC}/work/icf"/*; do
+  if  [[ ! "$file" =~ "main.zsh" ]]; then
+    if [[ -f "$file" ]] && [[ -r "$file" ]]; then
+      source "$file"
+    fi
+  fi
+done
