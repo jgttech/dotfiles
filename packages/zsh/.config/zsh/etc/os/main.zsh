9491ba8310818ebfddb435661881d24f3f9402ba
Jonathon Tech <59492@icf.com>
2025-05-16 09:58:51 -0500

    Update the structure of the dotfiles for ZSH

--- packages/zsh/.config/zsh/etc/os/main.zsh
+++ packages/zsh/.config/zsh/etc/os/main.zsh
@@ -2,7 +2,7 @@
 export PATH="$HOME/.config/nvim/bin:$PATH"
 
 if [[ $(uname) == "Darwin" ]]; then
-  source "${DOTFILES_ZSHRC}/os/darwin.zsh"
+  source "${DOTFILES_ZSHRC}/etc/os/darwin.zsh"
 elif command -v pacman &> /dev/null; then
-  source "${DOTFILES_ZSHRC}/os/archlinux.zsh"
+  source "${DOTFILES_ZSHRC}/etc/os/archlinux.zsh"
 fi
