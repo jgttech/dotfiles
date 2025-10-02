3bc850ecbf33b266d78da0f1c0c83e6140d66655
Jonathon Tech <jgt.tech@protonmail.com>
2025-10-02 12:28:46 -0500

    Update Python nvim venv-selector to latest version

--- packages/nvim/.config/nvim/lua/techvim/plugins/venv-selector.lua
+++ packages/nvim/.config/nvim/lua/techvim/plugins/venv-selector.lua
@@ -2,17 +2,14 @@ return {
   "linux-cultist/venv-selector.nvim",
   dependencies = {
     "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap",
-    "mfussenegger/nvim-dap-python", --optional
-    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
   },
-  lazy = false,
-  branch = "main",
+  ft = "python",                                                                                       -- Load when opening Python files
   keys = {
-    { ",v", "<cmd>VenvSelect<cr>" },
+    { ",v", "<cmd>VenvSelect<cr>" },                                                                   -- Open picker on keymap
   },
-  ---@type venv-selector.Config
-  opts = {
-    -- Your settings go here
+  opts = {                                                                                             -- this can be an empty lua table - just showing below for clarity.
+    search = {},                                                                                       -- if you add your own searches, they go here.
+    options = {}                                                                                       -- if you add plugin options, they go here.
   },
 }
