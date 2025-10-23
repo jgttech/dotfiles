868115d9f43056508174ec0784c8bc49300f36ba
Jonathon Tech <jgt.tech@protonmail.com>
2025-10-23 10:33:57 -0500

    Update Neovim

--- packages/nvim/.config/.old/nvim/init.lua
+++
@@ -1,2 +0,0 @@
--- require("config.lazy")
-require("techvim.pm.lazy")
--- packages/nvim/.config/.old/nvim/lua/techvim/local/tailwind-tools.nvim/queries/html/class.scm
+++
@@ -1,5 +0,0 @@
-(attribute
-  (attribute_name) @_attribute_name
-  (#eq? @_attribute_name "class")
-  (quoted_attribute_value
-    (attribute_value) @tailwind))
