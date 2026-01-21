c088c6c7255c47c27a8b424d8d2dd552856d2b2f
Jonathon Tech <jgt.tech@protonmail.com>
2026-01-21 12:22:50 -0600

    Disable blamer.nvim plugin
    The blamer.nvim plugin has been disabled by returning an empty table
    instead of the plugin configuration. This change was made to remove
    the inline git blame functionality from the Neovim editor.
    
    [Affected Files]
    - packages/nvim/.config/nvim/lua/plugins/blamer.lua

--- packages/nvim/.config/nvim/lua/plugins/blamer.lua
+++ packages/nvim/.config/nvim/lua/plugins/blamer.lua
@@ -1,6 +1,8 @@
-return {
-  "APZelos/blamer.nvim",
-  config = function()
-    vim.g.blamer_enabled = true
-  end,
-}
+return {}
+
+-- return {
+--   "APZelos/blamer.nvim",
+--   config = function()
+--     vim.g.blamer_enabled = true
+--   end,
+-- }
