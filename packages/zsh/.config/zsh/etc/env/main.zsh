ac6202a1e792d26eb3eb9cb902181a85056d081f
Jon Tech <jon@securacore.net>
2026-05-04 14:04:39 -0500

    Update config

--- packages/zsh/.config/zsh/etc/env/main.zsh
+++ packages/zsh/.config/zsh/etc/env/main.zsh
@@ -2,5 +2,9 @@
 export WORK_ID="FJ24MP6LC1"
 export WORK_OS="Darwin"
 
+# Turn off Claude's memory tracking. It causes
+# faulty commands to be remembered.
+export CLAUDE_CODE_DISABLE_AUTO_MEMORY=1
+
 alias vim="nvim"
-alias opencode="ocv"
+# alias opencode="ocv"
