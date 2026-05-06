50317df1fb9df5f0ba573ce25bc332d450fa30d4
kronos <jgt.tech@protonmail.com>
2026-05-06 00:35:06 -0500

    Add --build flag to install recipe script

--- bin/scripts/install/main.sh
+++ bin/scripts/install/main.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
+set -euo pipefail
+build=false
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --build) build=true; shift ;;
+    *) shift ;;
+  esac
+done
+
+if $build; then
+  just build
+fi
+
 dotfiles install
